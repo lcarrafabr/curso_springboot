d9c6f765f949c0d887ef9cca942b99030db491e8
lcarrafabr <lcarrafa.br@gmail.com>
2019-08-03 19:06:34 -0300

    Cadastrando pessoa, listando e inserindo materialize

--- springboot/src/main/java/curso/springboot/controller/PessoaController.java
+++ springboot/src/main/java/curso/springboot/controller/PessoaController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import curso.springboot.model.Pessoa;
 import curso.springboot.repository.PessoaRepository;
@@ -21,13 +22,34 @@ public class PessoaController {
 		return "cadastro/cadastropessoa";
 	}
 	
-	/**Metodo para salvar*/
+	/**Metodo para salvar
+	 * Com as alterações salva e ja mostra na tela.
+	 * Se for um cadastro onde não tenha uma table, usar o que está comentado*/
 	@RequestMapping(method=RequestMethod.POST, value="/salvarpessoa")
-	public String salvar(Pessoa pessoa) {
+	//public String salvar(Pessoa pessoa) {
+	public ModelAndView salvar(Pessoa pessoa) {
 		
 		pessoaRepository.save(pessoa);
 		
-		return "cadastro/cadastropessoa";//Apos salvar retorne para a mesma pagina
+		//-------------------- Essa parte so existe depois que existir o model and view ---------------------
+		ModelAndView andView = new ModelAndView("cadastro/cadastropessoa");//Vou manter na tela cadastro/cadastropessoa
+		Iterable<Pessoa> pessoasIt = pessoaRepository.findAll();
+		andView.addObject("pessoas", pessoasIt);//pessoas está no html <tr th:each = "pessoa : ${pessoas}">
+		//-----------------------------------------------------------------------------------------------------
+		
+		//return "cadastro/cadastropessoa";//Apos salvar retorne para a mesma pagina
+		return andView;
+	}
+	
+	/**ModelAndView liga o modelo a tela (estou usando para fazer a lista*/
+	@RequestMapping(method=RequestMethod.GET, value="/listapessoas")
+	public ModelAndView pessoas() {
+		
+		ModelAndView andView = new ModelAndView("cadastro/cadastropessoa");//Vou manter na tela cadastro/cadastropessoa
+		Iterable<Pessoa> pessoasIt = pessoaRepository.findAll();
+		andView.addObject("pessoas", pessoasIt);//pessoas está no html <tr th:each = "pessoa : ${pessoas}">
+		
+		return andView;
 	}
 	
 }
