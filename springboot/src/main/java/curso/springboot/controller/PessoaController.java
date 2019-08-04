dd543659844ec389619caa591350df14527a99a8
lcarrafabr <lcarrafa.br@gmail.com>
2019-08-04 18:17:05 -0300

    Validações pelo lado do servidor (apenas cadastro pessoas)

--- springboot/src/main/java/curso/springboot/controller/PessoaController.java
+++ springboot/src/main/java/curso/springboot/controller/PessoaController.java
@@ -1,9 +1,15 @@
 package curso.springboot.controller;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -48,7 +54,27 @@ public class PessoaController {
 	 * Se for um cadastro onde não tenha uma table, usar o que está comentado*/
 	@RequestMapping(method=RequestMethod.POST, value="**/salvarpessoa")//Dois ** ignoram tudo o que existe antes (util para editar)
 	//public String salvar(Pessoa pessoa) {
-	public ModelAndView salvar(Pessoa pessoa) {
+	//public ModelAndView salvar(Pessoa pessoa) {//Sem validador
+	public ModelAndView salvar(@Valid Pessoa pessoa, BindingResult bindingResult) {//Com validador
+		
+		
+		/**Se houver validador se campos vaios usar as linhas do IF*/
+		if(bindingResult.hasErrors()) {
+			
+			ModelAndView modelAndView = new ModelAndView("cadastro/cadastropessoa");
+			Iterable<Pessoa> pessoasIt = pessoaRepository.findAll();
+			modelAndView.addObject("pessoas", pessoasIt);//pessoas está no html <tr th:each = "pessoa : ${pessoas}">
+			modelAndView.addObject("pessoaobj", pessoa);
+			
+			List<String> msg = new ArrayList<String>();
+			
+			for(ObjectError objectError : bindingResult.getAllErrors()) {
+				msg.add(objectError.getDefaultMessage()); //O getDefaultMessage pega as mensagem das anotações nos models @NotEmpty e outras
+			}
+			
+			modelAndView.addObject("msg", msg);
+			return modelAndView;
+		}
 		
 		pessoaRepository.save(pessoa);
 		
--- springboot/src/main/java/curso/springboot/model/Pessoa.java
+++ springboot/src/main/java/curso/springboot/model/Pessoa.java
@@ -10,6 +10,9 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 @Entity
 public class Pessoa implements Serializable {
@@ -24,14 +27,20 @@ public class Pessoa implements Serializable {
 	private Long id;
 	
 	@Column(length=100)
+	@NotNull(message="O nome não pode ser nulo")
+	@NotEmpty(message="O campo nome não pode ser vazio")
 	private String nome;
+	
 	@Column(length=210)
+	@NotNull(message="O sobrenome não pode ser nulo")
+	@NotEmpty(message="O campo sobrenome não pode ser vazio")
 	private String sobrenome;
 	
 	/**Criar essa lista após criar a classe de relação. Se um para muitos, criar essa linha após existir a classe muitos*/
 	@OneToMany(mappedBy="pessoa", orphanRemoval=true, cascade=CascadeType.ALL) //MappedBy mapeia para o nome da variavel em que criou na tela telefones (private Pessoa pessoa;)
 	private List<Telefone> telefones; //Usado para relacionar um para muitos (no caso uma pessoa para muitos telefones)
 	
+	@Min(value = 18, message="A idade não pode ser menor que 18 anos")
 	private int idade;
 
 	public Long getId() {
